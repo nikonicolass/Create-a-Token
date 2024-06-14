b8b387d8b2dc274b765856c07cf06dbff6b7aba7
nikonicolass <165890009+nikonicolass@users.noreply.github.com>
2024-06-15 04:19:00 +0800

    Update index (1).js

--- index (1).js
+++ index (1).js
@@ -5,19 +5,19 @@ contract MyToken {
     // public variables here
     string public name = "nikko";
     string public symbol = "NIK";
-    unit public totalSupply = 0;
+    uint public totalSupply = 0;
     
     // mapping variable here
-    mapping(address => nint) public balances;
+    mapping(address => uint) public balances;
     
     // mint function
-    function mint (address _address, nint _value) public{
+    function mint (address _address, uint _value) public{
         totalSupply += _value;
         balances[_address] += _value;
     }
     
     // burn function
-    function burn (address _address, nint _value) public{
+    function burn (address _address, uint _value) public{
         if(balances[_address] >= _value){
           totalSupply -= _value;
           balances[_address] -= _value;
